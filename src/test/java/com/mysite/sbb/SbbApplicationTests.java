f10a53bd4ad35d4d6ff5bc479b75289788c9f047
changluck <changluck@hotmail.com>
2026-06-17 17:26:21 +0900

    두번째 커밋

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,95 @@
 package com.mysite.sbb;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 class SbbApplicationTests {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    //질문 데이터를 통해 답변 데이터 찾기
+    @Transactional
     @Test
-    void contextLoads() {
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(4);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 
+/*
+    //질문 데이터 삭제하기
+    @Test
+    void testJpa() {
+        assertEquals(2, this.questionRepository.count());
+        Optional<Question> oq = this.questionRepository.findById(3);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
+    }
+*/
+
+/*
+    void testJpa() {
+        Optional<Question> question = questionRepository.findById(3);
+        assertTrue(question.isPresent());
+        Question qe = question.get();
+        qe.setSubject("수정된 제목");
+        this.questionRepository.save(qe);
+    }
+*/
+
+/*    void testJpa() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(3, q.getId());
+    }*/
+
+    /*
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);  // 첫번째 질문 저장
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);  // 두번째 질문 저장
+    }
+     */
+
+
+    /*
+    void testJpa() {
+        List<Question> questionList = this.questionRepository.findAll();
+        assertEquals(4,questionList.size());
+
+        Question question = questionList.get(0);
+        assertEquals("sbb에 대해서 알고 싶습니다.", question.getContent());
+    }
+    */
 }
